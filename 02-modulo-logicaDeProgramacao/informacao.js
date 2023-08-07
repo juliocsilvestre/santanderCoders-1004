0c74872cab0b2a0fe441a2f197469dafc478eb4b
Julio Silvestre <jcss.silvestre@gmail.com>
2023-08-06 23:40:53 -0300

    update file extension

--- 02-modulo-logicaDeProgramacao/informacao.js
+++ 02-modulo-logicaDeProgramacao/informacao.js
@@ -1,4 +1,4 @@
-Entrega das Listas
+/* Entrega das Listas
 - Entregar em um único arquivo javascript com o nome da lista.
   
   Exemplo: lista-01
@@ -10,4 +10,4 @@ Sugestão:
 Criar um repositório específico da nossa matérias criar a estrutura de pastas abaixo
 
 - Listas
-- Projeto Final
+- Projeto Final */
